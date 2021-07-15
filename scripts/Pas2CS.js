2f6c09bdc44adf09e9baa5729f2cb4f0993e98a4
Vlad <modproject@ukr.net>
2021-07-15 15:19:40 +0300

    data-types fixed bug

--- scripts/Pas2CS.js
+++ scripts/Pas2CS.js
@@ -11,7 +11,6 @@ function Pascal2CSharp()
   `
     + text_area1.innerHTML
       .replace(/uses(.+?);/gi, "")
-
       // Операторы
       .replace(/\bif\b(.+?)=(.+?)then/gi, "if $1==$2 then")
       .replace(/\bif\b\s*\((.+?)\)/gi, "if $1")
@@ -23,17 +22,14 @@ function Pascal2CSharp()
       .replace(/&lt;&gt;/g,"!=")
       .replace(/{(.+?)}/g, "/* $1 */")
       .replace(/'(.+?):(.+?)'/g, "\'$1:&$500$2\'") // &$500 Временный символ
-      .replace(/:(?!&\$500)/g, "")
+      
 
       // ...
       .replace(/program (.+?);/i, "public class $1{")
-      .replace(/begin/i, 
-      `\n<li>public static void Main(string[] args)</li>
-      <li>{</li>
-      `)
       .replace(/'/gi, "\"")
 
       // Циклы
+      .replace(/\bfor\b(.+?)\bdo\s+begin/gi, "for($1){&$501") // &$501 Временный символ
       .replace(/\bfor\b(.+?)\bdo\b/gi, "for($1)&$501") // &$501 Временный символ
       .replace(/for\((.+?)=(.+?)\bto\b(.+?)\)&\$501/gi,"for($1=$2; $1<$3; $1++)&$501")
       .replace(/for\((.+?)=(.+?)\bdownto\b(.+?)\)&\$501/gi, "for($1=$2; $1>$3; $1--)&$501")
@@ -46,38 +42,40 @@ function Pascal2CSharp()
       .replace(/\bforeach\s*var\b(.+?)\bdo\b/gi, "foreach(&$501var$1)")
 
       // Data-Types
-      .replace(/\bvar\b/gi, "&$503")
-      .replace(/\btype\b/gi, "Type&$503")
-      .replace(/Integer;<\/li>/gi, "Integer;</li><li>&$503</li>")
-      .replace(/Real;<\/li>/gi, "Real;</li><li>&$503</li>")
-      .replace(/Boolean;<\/li>/gi,"Boolean;</li><li>&$503</li>")
-      .replace(/Char;<\/li>/gi, "Char;</li><li>&$503</li>")
-      .replace(/String;<\/li>/gi, "String;</li><li>&$503</li>")
+      .replace(/<li>\s*var\s*<\/li>/gi, "\n")
+      .replace(/\bvar\b/gi, "")
+      .replace(/\btype\b/gi, "Type")
+      .replace(/:\s*Integer;<\/li>/gi, "Integer;</li>\n<li>&$503</li>\n")
+      .replace(/:\s*Real;<\/li>/gi, "Real;</li>\n<li>&$503</li>\n")
+      .replace(/:\s*Boolean;<\/li>/gi,"Boolean;</li>\n<li>&$503</li>\n")
+      .replace(/:\s*Char;<\/li>/gi, "Char;</li>\n<li>&$503</li>\n")
+      .replace(/:\s*String;<\/li>/gi, "String;</li>\n<li>&$503</li>\n")
 
-      .replace(/<li>&\$503<\/li><li>(.+?)Integer;<\/li>/gi, "<li>static int $1;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)Real;<\/li>/gi, "<li>static float $1;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)Boolean;<\/li>/gi,"<li>static bool $1;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)Char;<\/li>/gi, "<li>static char $1;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)String;<\/li>/gi, "<li>static string $1;</li><li>&$503</li>")
-      
-      .replace(/<li>&\$503<\/li><li>(.+?)Integer(.+?);<\/li>/gi, "<li>static int $1$2;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)Real(.+?);<\/li>/gi, "<li>static float $1$2;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)Boolean(.+?);<\/li>/gi,"<li>static bool $1$2;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)Char(.+?);<\/li>/gi, "<li>static char $1$2;</li><li>&$503</li>")
-      .replace(/<li>&\$503<\/li><li>(.+?)String(.+?);<\/li>/gi, "<li>static string $1$2;</li><li>&$503</li>")
+      .replace(/<li>(.+?):\s*Integer;<\/li>\n<li>&\$503<\/li>/gi, "<li>static int $1;</li>")
+      .replace(/<li>(.+?):\s*Real;<\/li>\n<li>&\$503<\/li>/gi, "<li>static float $1;</li>")
+      .replace(/<li>(.+?):\s*Boolean;<\/li>\n<li>&\$503<\/li>/gi,"<li>static bool $1;</li>")
+      .replace(/<li>(.+?):\s*Char;<\/li>\n<li>&\$503<\/li>/gi, "<li>static char $1;</li>")
+      .replace(/<li>(.+?):\s*String;<\/li>\n<li>&\$503<\/li>/gi, "<li>static string $1;</li>")
+
+      .replace(/:(?!&\$500)/g, "")
+      // .replace(/<li>&\$503<\/li><li>(.+?)Integer(.+?);<\/li>/gi, "<li>static int $1$2;</li><li>&$503</li>")
+      // .replace(/<li>&\$503<\/li><li>(.+?)Real(.+?);<\/li>/gi, "<li>static float $1$2;</li><li>&$503</li>")
+      // .replace(/<li>&\$503<\/li><li>(.+?)Boolean(.+?);<\/li>/gi,"<li>static bool $1$2;</li><li>&$503</li>")
+      // .replace(/<li>&\$503<\/li><li>(.+?)Char(.+?);<\/li>/gi, "<li>static char $1$2;</li><li>&$503</li>")
+      // .replace(/<li>&\$503<\/li><li>(.+?)String(.+?);<\/li>/gi, "<li>static string $1$2;</li><li>&$503</li>")
 
       //.replace(/&\$503/gi,'')
 
       // Массивы
-      .replace(/<li>(.+?)\barray\s*\[-(.+?)..(.+?)\]\s*of\s*integer;/gi, "<li>$1int[] = new int[$2+$3];")
-      .replace(/<li>(.+?)\barray\s*\[(.+?)..-(.+?)\]\s*of\s*integer;/gi, "<li>$1int[] = new int[$2+$3];")
-      .replace(/<li>(.+?)\barray\s*\[-(.+?)..-(.+?)\]\s*of\s*integer;/gi, "<li>$1int[] = new int[$2+$3];")
-      .replace(/<li>(.+?)\barray\s*\[(.+?)..(.+?)\]\s*of\s*integer;/gi, "<li>$1int[] = new int[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[-(.+?)..(.+?)\]\s*of\s*integer;/gi, "<li>int[] $1= new int[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[(.+?)..-(.+?)\]\s*of\s*integer;/gi, "<li>int[] $1= new int[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[-(.+?)..-(.+?)\]\s*of\s*integer;/gi, "<li>int[] $1= new int[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[(.+?)..(.+?)\]\s*of\s*integer;/gi, "<li>int[] $1= new int[$2+$3];")
 
-      .replace(/<li>(.+?)\barray\s*\[-(.+?)..(.+?)\]\s*of\s*real;/gi, "<li>$1float[] = new float[$2+$3];")
-      .replace(/<li>(.+?)\barray\s*\[(.+?)..-(.+?)\]\s*of\s*real;/gi, "<li>$1float[] = new float[$2+$3];")
-      .replace(/<li>(.+?)\barray\s*\[-(.+?)..-(.+?)\]\s*of\s*real;/gi, "<li>$1float[] = new float[$2+$3];")
-      .replace(/<li>(.+?)\barray\s*\[(.+?)..(.+?)\]\s*of\s*real;/gi, "<li>$1float[] = new float[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[-(.+?)..(.+?)\]\s*of\s*real;/gi, "<li>float[] $1= new float[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[(.+?)..-(.+?)\]\s*of\s*real;/gi, "<li>float[] $1= new float[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[-(.+?)..-(.+?)\]\s*of\s*real;/gi, "<li>float[] $1= new float[$2+$3];")
+      .replace(/<li>(.+?)\barray\s*\[(.+?)..(.+?)\]\s*of\s*real;/gi, "<li>float[] $1= new float[$2+$3];")
     
       // Циклы
       .replace(/foreach\(&\$501var(.+?)\)/gi, "foreach(var$1)")
@@ -98,7 +96,7 @@ function Pascal2CSharp()
       .replace(/readln\(\);|readln\(\)|readln;|readln/gi, "Console.ReadLine();") 
       
       // ...
-      .replace(/end;|end.|end/gi, "}")
+      .replace(/\bend;|\bend(?!.)/gi, "}")
 
       // Операторы, operators
       .replace(/\bdiv\b/gi, "/")
@@ -121,8 +119,12 @@ function Pascal2CSharp()
       .replace(/\bread\(\);|\bread\(\)|read;|\bread\b/gi,"Console.Read();")
       .replace(/<li><\/li>(.+?)Char(.+?);/gi,"<li>static char $1 $2;</li><li></li>")
       .replace(/char (.+?)\s*=\s*"(.+?)"/gi, "char $1 = '$2'")
-      .replace(/begin/gi, "{")
+      //.replace(/begin/gi, "{")
       .replace(/<>/gm, "!=")
+      .replace(/<li>\s*begin(.+?)end.\s*<\/li>/i, 
+      `\n<li>public static void Main(string[] args)</li>\n
+      <li>{</li>$1<li>}</li>
+      `)
     +`<li>}</li>
     <li>}</li>
     `;
